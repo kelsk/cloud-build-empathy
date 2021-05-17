a1281774130e267679011ba22ee34ec88360fcdc
kelsk <38271546+kelsk@users.noreply.github.com>
2021-05-17 12:37:36 -0700

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     name = os.environ.get("NAME", "World")
-    return "Hello there, {}!".format(name)
+    return "Helloooo there, {}!".format(name)
 
 
 if __name__ == "__main__":
